4ccb84685b3e162f7cc86e4b593d924ffecfbde7
Justin Bull <justin@wealthsimple.com>
2018-10-27 13:13:58 -0400

    Extremely MVP attempt

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # go-geolocator
-Go + Echo + Maxmind's GeoIP2 DB
+
+A true micro-service for geolocating IP addresses using Go for speed and MaxMind's GeoIP2 City database.
+
+## Setup
+
+You must first download the geolite databases for local development!
+
+```bash
+git clone git@github.com:f3ndot/go-geolocator.git
+cd go-geolocator
+\curl -SL https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz | tar xvz --strip-components 1 -C data/ '*/*.mmdb'
+\curl -SL https://geolite.maxmind.com/download/geoip/database/GeoLite2-ASN.tar.gz | tar xvz --strip-components 1 -C data/ '*/*.mmdb'
+```
+
+## Licenses
+
+### `go-geolocator`
+
+See the [LICENSE](license) file.
+
+### GeoLite2
+
+The GeoLite2 databases are distributed under the [Creative Commons Attribution-ShareAlike 4.0 International License](https://creativecommons.org/licenses/by-sa/4.0/). The attribution requirement may be met by including the following in all advertising and documentation mentioning features of or use of this database:
+
+```html
+This product includes GeoLite2 data created by MaxMind, available from
+<a href="http://www.maxmind.com">http://www.maxmind.com</a>.
+```
+
+### `geoip2-golang`
+
+```
+ISC License
+
+Copyright (c) 2015, Gregory J. Oschwald <oschwald@gmail.com>
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
+```
--- server.go
+++ server.go
@@ -1,33 +1,122 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	geoip2 "github.com/oschwald/geoip2-golang"
+	maxminddb "github.com/oschwald/maxminddb-golang"
 )
 
+// LocationResponse is the JSON response structure containing the geographic
+// location information
 type LocationResponse struct {
-	ID        string `json:"id"`
-	RequestID string `json:"request_id"`
+	ID          string `json:"id"`
+	RequestID   string `json:"request_id"`
+	Copyright   string `json:"copyright_notice"`
+	geoip2.City `json:"geolocation"`
 }
 
-func lookupIdentifier(c echo.Context) error {
+type LocationSummaryResponse struct {
+	ID        string `json:"id"`
+	RequestID string `json:"request_id"`
+	Copyright string `json:"copyright_notice"`
+	City      string `json:"geolocation"`
+}
+
+func geolocate(ipAddress string) *geoip2.City {
+	db, err := geoip2.Open(os.Getenv("CITY_MMDB_PATH"))
+	if err != nil {
+		panic(fmt.Sprintf("Unable to open geoIP2 database: %s", err))
+	}
+	defer db.Close()
+	ip := net.ParseIP(ipAddress)
+
+	record, err := db.City(ip)
+	if err != nil {
+		panic(fmt.Sprintf("Unable to lookup city: %s", err))
+	}
+	return record
+}
+
+func geolocateSummary(ipAddress string) string {
+	db, err := maxminddb.Open(os.Getenv("CITY_MMDB_PATH"))
+	if err != nil {
+		panic(fmt.Sprintf("Unable to open geoIP2 database: %s", err))
+	}
+	defer db.Close()
+	ip := net.ParseIP(ipAddress)
+
+	var record struct {
+		Country struct {
+			Names map[string]string `maxminddb:"names"`
+		} `maxminddb:"country"`
+	}
+
+	err = db.Lookup(ip, &record)
+	if err != nil {
+		panic(fmt.Sprintf("Unable to lookup: %s", err))
+	}
+	return record.Country.Names["en"]
+}
+
+func handleIdentifierLookup(c echo.Context) error {
 	id := c.Param("id")
 	reqID := c.Response().Header().Get(echo.HeaderXRequestID)
-	// summary := c.QueryParam("summary")
-	return c.JSON(http.StatusOK, &LocationResponse{
+
+	if c.QueryParam("summary") != "" {
+		return c.JSONPretty(http.StatusOK, &LocationSummaryResponse{
+			ID:        id,
+			City:      geolocateSummary(id),
+			RequestID: reqID,
+			Copyright: "This product includes GeoLite2 data created by MaxMind, available from http://www.maxmind.com",
+		}, "  ")
+	}
+	return c.JSONPretty(http.StatusOK, &LocationResponse{
 		ID:        id,
+		City:      *geolocate(id),
 		RequestID: reqID,
-	})
+		Copyright: "This product includes GeoLite2 data created by MaxMind, available from http://www.maxmind.com",
+	}, "  ")
+}
+
+func loadEnv() {
+	env := os.Getenv("GEOLOCATOR_ENV")
+	if "" == env {
+		env = "development"
+		if os.Setenv("GEOLOCATOR_ENV", env) != nil {
+			log.Fatal("Unable to set GEOLOCATOR_ENV to sane default")
+		}
+	}
+	godotenv.Load(".env." + env + ".local")
+	if "test" != env {
+		godotenv.Load(".env.local")
+	}
+	godotenv.Load(".env." + env)
+	godotenv.Load() // The Original .env
+}
+
+func validateEnvVars() {
+	if os.Getenv("CITY_MMDB_PATH") == "" {
+		log.Fatal("CITY_MMDB_PATH not set. Did you follow the README?")
+	}
 }
 
 func main() {
+	loadEnv()
+	validateEnvVars()
+
 	e := echo.New()
 	e.Use(middleware.Recover())
 	e.Use(middleware.Gzip())
 	e.Use(middleware.RequestID())
 	e.Use(middleware.Logger())
-	e.GET("/api/location/:id", lookupIdentifier)
+	e.GET("/api/location/:id", handleIdentifierLookup)
 	e.Logger.Fatal(e.Start(":1323"))
 }
